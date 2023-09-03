77724760c971ee67136dd3259bbb9c10563ff435
Brayden Smith <139816922+belaxed@users.noreply.github.com>
2023-09-03 15:32:16 -0400

    Pass tests

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,38 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    if (!Number.isInteger(year)) return "ERROR";
+    if (year < 0) return "ERROR";
 
+    let isDivisibleByFour;
+    let isDivisibleByFourHundred;
+    let isDivisibleByOneHundred;
+
+    if (year % 4 == 0) {
+        isDivisibleByFour = true;
+    }
+    if (year % 400 == 0) {
+        isDivisibleByFourHundred = true;
+    }
+    if (year % 100 == 0) {
+        isDivisibleByOneHundred = true;
+    }
+
+    if (isDivisibleByFourHundred) {
+        return true;
+    }
+    else {
+        if (isDivisibleByOneHundred) {
+            return false;
+        }
+        else {
+            if (isDivisibleByFour) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+    }
+    
 };
 
 // Do not edit below this line
