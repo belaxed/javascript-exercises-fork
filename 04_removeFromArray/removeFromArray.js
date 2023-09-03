bfee150e5da5b4400a58ff89f4c926b06ffe7022
Brayden Smith <139816922+belaxed@users.noreply.github.com>
2023-09-03 14:39:56 -0400

    Remove unecessary comment

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,4 @@
 const removeFromArray = function(array, ...removals) {
-    /*
-    for (i = 0; i < args.length; i++) {
-        if (indexOf(...args) !== -1) {
-            array.splice(array.indexOf(arguments[i + 1]),1);
-        }
-        else {
-            continue;
-        }
-        
-    }
-    
-    return array;
-    */
 
     for (let i = 0; i < removals.length; i++) {
         if (array.includes(removals[i])) {
