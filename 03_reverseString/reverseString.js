088a0f120fdb6cfb1fb72df03161ced6be71c992
Brayden Smith <139816922+belaxed@users.noreply.github.com>
2023-09-03 11:33:02 -0400

    Pass tests

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    if (typeof string === "string") {
+        const splitArray = string.split("");
+        const reversedArray = splitArray.reverse();
+        let reversedString = reversedArray.join("")
 
+        return reversedString;
+    }
+    else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
