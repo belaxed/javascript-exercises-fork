08d8671dbd8a2c710367881559f5f97b7da4fd72
Brayden Smith <139816922+belaxed@users.noreply.github.com>
2023-09-03 14:38:57 -0400

    Pass tests

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,28 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removals) {
+    /*
+    for (i = 0; i < args.length; i++) {
+        if (indexOf(...args) !== -1) {
+            array.splice(array.indexOf(arguments[i + 1]),1);
+        }
+        else {
+            continue;
+        }
+        
+    }
+    
+    return array;
+    */
 
+    for (let i = 0; i < removals.length; i++) {
+        if (array.includes(removals[i])) {
+            array.splice(array.indexOf(removals[i]), 1);
+        }
+        else {
+            continue;
+        }
+    }
+
+    return array;
 };
 
 // Do not edit below this line
