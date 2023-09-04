a63404a60f263ba155656790d2eeee7970d3011e
Brayden Smith <139816922+belaxed@users.noreply.github.com>
2023-09-04 17:05:07 -0400

    Pass tests

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let strLower = str.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~() ]/g,"");
+    console.log(strLower);
+    let strSplit = strLower.split("");
+    console.log(strSplit);
+    let strReverseSplit = strSplit.reverse();
+    console.log(strReverseSplit);
+    let strReverse = strReverseSplit.join("");
+    console.log(strReverse);
+    if (strLower == strReverse) return true;
+    else return false;
 };
 
 // Do not edit below this line
